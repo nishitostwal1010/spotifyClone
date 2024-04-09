d0a9ae84eefa08079831e96a506beee479ac8f69
nishitostwal1010 <157974203+nishitostwal1010@users.noreply.github.com>
2024-04-10 00:01:08 +0530

    Update index.js

--- index.js
+++ index.js
@@ -38,7 +38,7 @@ function convertSecondsToMinutesAndSeconds(seconds) {
 }
 
 async function getSongs() {
-    let a = await fetch("http://127.0.0.1:5500/songs/");
+    let a = await fetch("/songs/");
     let response = await a.text();
     let div = document.createElement("div");
     div.innerHTML = response;
